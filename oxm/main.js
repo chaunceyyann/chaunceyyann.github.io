c6a2746d74f666c94fc49015f756c6bd02c1e837
cyanjin <chaunceyyann@gmail.com>
2015-04-20 03:26:33 -0700

    Dummy OxygenMonitor Page finished

---
+++ oxm/main.js
@@ -0,0 +1,69 @@
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart_ox);
+function drawChart_ox() {
+  var data_ox = google.visualization.arrayToDataTable([
+	  ['Time', 'Oxygen'],
+	  [ 1,       22],
+	  [ 5,       25.5],
+	  [ 10,      24],
+	  [ 14,      25],
+	  [ 20,      23.5],
+	  [ 23,      27]
+	  ]);
+
+  var options_ox = {
+	hAxis: {title: 'Time/h', minValue: 00, maxValue: 24},
+	vAxis: {title: 'Oxygen/%', minValue: 15, maxValue: 30},
+	legend: 'none'
+  };
+
+  var chart1 = new google.visualization.ScatterChart(document.getElementById('chart_div1'));
+  chart1.draw(data_ox, options_ox);
+}
+
+
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart_hu);
+function drawChart_hu() {
+  var data_hu = google.visualization.arrayToDataTable([
+	  ['Time', 'Humidity'],
+	  [ 1,       2223],
+	  [ 5,       2055],
+	  [ 10,      2012],
+	  [ 14,      2132],
+	  [ 20,      2352],
+	  [ 23,      2302]
+	  ]);
+
+  var options_hu = {
+	hAxis: {title: 'Time/h', minValue: 00, maxValue: 24},
+	vAxis: {title: 'Humidity', minValue: 2000, maxValue: 2500},
+	legend: 'none'
+  };
+
+  var chart2 = new google.visualization.ScatterChart(document.getElementById('chart_div2'));
+  chart2.draw(data_hu, options_hu);
+}
+
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart_tp);
+function drawChart_tp() {
+  var data_tp = google.visualization.arrayToDataTable([
+	  ['Time', 'Temperature'],
+	  [ 1,       25],
+	  [ 5,       25.5],
+	  [ 10,      19],
+	  [ 14,      22],
+	  [ 20,      23.5],
+	  [ 23,      17]
+	  ]);
+
+  var options_tp = {
+	hAxis: {title: 'Time/h', minValue: 00, maxValue: 24},
+	vAxis: {title: 'Temperature/C degree', minValue: 15, maxValue: 30},
+	legend: 'none'
+  };
+
+  var chart3 = new google.visualization.ScatterChart(document.getElementById('chart_div3'));
+  chart3.draw(data_tp, options_tp);
+}
