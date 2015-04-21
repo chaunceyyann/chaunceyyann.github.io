8a5e9cf43422d16525362c8f7271699cd3e521f1
cyanjin <chaunceyyann@gmail.com>
2015-04-20 18:26:28 -0700

    Finished Home and contact with template and fixed autodoor position

---
+++ cgi-bin/door.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+import socket
+import cgi, cgitb 
+
+host = 'chauncity.onthewifi.com'
+port = 5555
+size = 1024
+data = 'ERROR'
+username = ''
+password = ''
+request = ''
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port))
+
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+username = form.getvalue('username')
+password  = form.getvalue('pin')
+request = form.getvalue('request')
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>Autodoor Website</title>'
+print '</head>'
+print '<body>'
+print '<h2>Contacting Earth to unlock the door, now.</h2>'
+
+if request == None:
+	data = 'Unsuported Browser.. for now sorry! :('
+
+if username != '' and password != '' and request != '' and request != None:
+	message = "<message><type>handshake</type><user>%s</user><pin>%s</pin></message>" % (username, password)
+	s.send(message)
+	data = s.recv(size)
+	#print "<p>User:%s, pin:%s, Request:(%s)</p>" % (username, password, request)
+		
+	if data == '<message> <type>handshake</type> <from>earth</from> </message>':
+		print "<h2>PASSED</h2>"
+		message = "<message> <type>%s</type> <user>%s</user> </message>" % (request, username)
+		s.send(message)
+		data = s.recv(size)
+	else:
+		print "<h2>FAILED</h2>"
+		print "<p>Invalid Username or Password.</p>"
+
+	s.close()
+else:
+	print "<h2>FAILED</h2>"
+	print "<p>%s: Invalid Request.</p>" % (data)
+print '</body>'
+print '</html>'
