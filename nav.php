978881362d6ca68d8ce23fd7fb04b0e14baafb71
cyanjin <chaunceyyann@gmail.com>
2016-12-08 16:29:07 -0800

    Reorganize root dir, testing php

---
+++ nav.php
@@ -0,0 +1,10 @@
+    <div class="header">
+      <div class="container">
+        <ul class="nav">
+          <li>About</li>
+          <li>Work</li>
+          <li>Team</li>
+          <li>Contact</li>  
+        </ul>
+      </div>
+    </div>
